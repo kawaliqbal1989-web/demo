05990eed6197cd8f35b91aabf4ead55cd72df7c7
kawaliqbal1989-web <kawaliqbal1989@gmail.com>
2026-03-13 13:16:23 +0530

    add

--- abacusweb.sql
+++ abacusweb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2026 at 08:15 AM
+-- Generation Time: Mar 13, 2026 at 08:42 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 

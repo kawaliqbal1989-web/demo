b23a9f1c36a2a369c36966ec5b4516af2d14d298
kawaliqbal1989-web <kawaliqbal1989@gmail.com>
2026-03-13 07:57:56 +0530

    update backend and database

--- src/app.js
+++ src/app.js
@@ -34,19 +34,18 @@ if (env.isProduction) {
 app.use(helmet());
 app.use(
   cors({
-    origin:defaultAllowedOrigins
-    // origin: (origin, callback) => {
-    //   if (!origin) {
-    //     return callback(null, true);
-    //   }
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
 
-    //   if (!env.isProduction) {
-    //     const ok = /^http:\/\/(localhost|127\.0\.0\.1):\d+$/i.test(origin);
-    //     return callback(null, ok);
-    //   }
+      if (!env.isProduction) {
+        const ok = /^http:\/\/(localhost|127\.0\.0\.1):\d+$/i.test(origin);
+        return callback(null, ok);
+      }
 
-    //   return callback(null, productionCorsAllowedOrigins.has(origin));
-    // },
+      return callback(null, productionCorsAllowedOrigins.has(origin));
+    },
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",

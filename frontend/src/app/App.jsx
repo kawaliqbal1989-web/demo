72f6e8fbef99add9988d5b76e4a90474f2ed106a
kawaliqbal1989-web <kawaliqbal1989@gmail.com>
2026-03-26 18:49:34 +0530

    frontend: wrap router with global error boundary

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,11 +1,14 @@
 import { Toaster } from "react-hot-toast";
 import { AppRouter } from "../routes/AppRouter";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 function App() {
   return (
     <>
       <Toaster position="top-right" toastOptions={{ duration: 3500 }} />
-      <AppRouter />
+      <ErrorBoundary>
+        <AppRouter />
+      </ErrorBoundary>
     </>
   );
 }
